4d85cf00988754791c8d305a70e01a474d8a50a3
Vata <vatanak.cambodge@gmail.com>
2026-09-08 17:11:05 +0700

    Deleted temp.py

--- temp.py
+++
@@ -1,6 +0,0 @@
-# Explicitly import the submodules or classes you need
-from PyQt6 import QtCore, QtWidgets
-
-# Now you can use them normally
-print("Qt Version:", QtCore.QT_VERSION_STR)
-app = QtWidgets.QApplication([])
